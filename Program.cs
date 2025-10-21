bdff9c69c2ca857020d5e8267a957dcd7abb77c3
Relander Jenny <jenny.relander@edu.prakticum.fi>
2025-10-21 15:34:54 +0300

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariablesInC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here I define a variable that stores a name
+            string name = "Jenny";
+            Console.WriteLine(name = "Jenny");
+
+            // Here I define a variable that stores a number
+            int num = 1;
+            Console.WriteLine(num = 1);
+
+            //  Here I define another variable with a different number
+            int num2 = 2;
+            Console.WriteLine(num = 2);
+            
+        }
+    }
+}
