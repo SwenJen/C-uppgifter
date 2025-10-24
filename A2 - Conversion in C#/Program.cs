b575322279b543a2f188adff69fd409d525a1427
SwenJen <jennyrelander@gmail.com>
2025-10-24 09:29:44 +0300

    Add files via upload

---
+++ A2 - Conversion in C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Conversion_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsString = "12345";
+            int number = int.Parse(numberAsString);
+            Console.WriteLine($"The converted number is: {number}");
+        }
+    }
+}
