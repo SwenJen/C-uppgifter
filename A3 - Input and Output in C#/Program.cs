be7521e3af9ab0d66679c89cd832123c6d2f30d2
SwenJen <jennyrelander@gmail.com>
2025-10-27 12:31:21 +0200

    Add files via upload

---
+++ A3 - Input and Output in C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Input_and_Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello, " + userName + "!");
+        }
+    }
+}
