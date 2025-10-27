13269f7635c6f7ea368e2edd73c7162db49949ec
SwenJen <jennyrelander@gmail.com>
2025-10-27 12:46:32 +0200

    Add files via upload

---
+++ A4 - If Statements in C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace If_Statements_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            int ageInput = Convert.ToInt32(age);
+
+            if(ageInput >= 18)
+            {
+                Console.WriteLine("You can vote.");
+            }
+            else
+            {
+                Console.WriteLine("You can not vote.");
+            }
+        }
+    }
+}
