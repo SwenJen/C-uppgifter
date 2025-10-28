04802117be633c74253ada1103a8e80635a03ea1
SwenJen <jennyrelander@gmail.com>
2025-10-28 12:17:03 +0200

    Add files via upload

---
+++ A6 - Loops in C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Loops_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // User input
+            Console.Write("What would you like to loop?: ");
+            string message = Console.ReadLine();
+
+            Console.Write("How many times?: ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= loopCounter; i++) 
+
+            Console.WriteLine(message);
+
+            Console.WriteLine("The loop has finished!");
+
+        }   
+    }    
+}
