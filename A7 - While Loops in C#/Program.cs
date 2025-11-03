72cb90d3af390d12be76d8a8cc506a7ad1084b87
SwenJen <jennyrelander@gmail.com>
2025-11-03 08:55:35 +0200

    Add files via upload

---
+++ A7 - While Loops in C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace While_Loops_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 5)
+            {
+                Console.WriteLine(counter++);
+                
+            }
+            Console.WriteLine("The loop has finished.");
+        }
+    }
+}
