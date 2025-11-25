be56f552992e68e9e868e83e67f23ee2c6896d7d
SwenJen <jennyrelander@gmail.com>
2025-11-25 08:59:46 +0200

    Add files via upload

---
+++ A_13 - Odd or Even C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_or_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            string numberInput = Console.ReadLine();
+            int number = Convert.ToInt32(numberInput); 
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number is even: {number % 2}");
+                
+            }
+            else
+            {
+                Console.WriteLine($"The number is odd: {number % 2}");
+            }
+
+            
+
+        }
+    }
+}
